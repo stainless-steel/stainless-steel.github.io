017b3e09a9d929e35a9e640d9a5ce948f68f8dd7
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-06 21:24:50 +0100

    Updated after_success.sh

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo pip install ghp-import &&
-ghp-import -n target/doc &&
+set -ev
+
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+
+sudo pip install ghp-import
+ghp-import -m 'Updated the documentation' -n target/doc
+
 git push -qf https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
