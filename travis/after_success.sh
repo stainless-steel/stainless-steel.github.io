40a9fef2f51efb0e05a7188f5d2a5942d3de6b65
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-06 22:32:58 +0100

    Generate index.html for the root directory

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -5,7 +5,14 @@ set -ev
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_EMAIL}"
 
+URL="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2)/index.html"
+INDEX="<!DOCTYPE html>
+<link rel='canonical' href='${URL}'>
+<meta http-equiv='refresh' content='0; url=${URL}'>
+<script>window.location='${URL}'</script>"
+
 cargo doc
+echo "${INDEX}" > target/doc/index.html
 
 sudo pip install ghp-import
 ghp-import -m 'Updated the documentation' -n target/doc
