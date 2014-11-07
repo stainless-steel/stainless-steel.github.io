029cab350dc48a7fdfa841935a27d5f3a1fd2eda
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-07 05:51:56 +0100

    Impersonalized after_success.sh

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -2,19 +2,20 @@
 
 set -ev
 
-git config user.name "${GIT_NAME}"
-git config user.email "${GIT_EMAIL}"
+git config user.name "Travis CI"
+git config user.email ""
 
 URL="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2)/index.html"
-INDEX="<!DOCTYPE html>
+
+HTML="<!DOCTYPE html>
 <link rel='canonical' href='${URL}'>
 <meta http-equiv='refresh' content='0; url=${URL}'>
 <script>window.location='${URL}'</script>"
 
 cargo doc
-echo "${INDEX}" > target/doc/index.html
+echo "${HTML}" > target/doc/index.html
 
 sudo pip install ghp-import
 ghp-import -m 'Updated the documentation' -n target/doc
 
-git push -qf https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git push -qf https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
