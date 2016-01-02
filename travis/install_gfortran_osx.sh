4aea82ecef5400bd3019c9a31e8bafed0a9733c3
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-02 11:43:31 +0100

    Refine the script for installing gfortran

--- travis/install_gfortran_osx.sh
+++ travis/install_gfortran_osx.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 set -ev
+
+ARCHIVE=gcc-5.3-bin.tar.gz
+curl -LO http://prdownloads.sourceforge.net/hpc/${ARCHIVE}
+
+mkdir gcc
+tar -xzf ${ARCHIVE} -C gcc
+
+ROOT="$PWD/gcc/local"
+export PATH="$ROOT/bin:$PATH"
+export DYLD_LIBRARY_PATH="$ROOT/lib:$DYLD_LIBRARY_PATH"
+
+gfortran --version
