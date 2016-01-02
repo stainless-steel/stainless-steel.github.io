ee2e0e26646c2b3ee62b27b42362ca10e65a4104
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-02 15:14:08 +0100

    Update install_gfortran.sh

--- travis/install_gfortran.sh
+++ travis/install_gfortran.sh
@@ -2,11 +2,11 @@
 
 set -ev
 
-if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
-  exit
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+  BREW_PATH=$(brew --prefix)
+  RUST_PATH=$(dirname $(dirname $(which rustc)))
+  LIBRARY_PATH=`find "${BREW_PATH}" -name libgfortran.dylib | grep -v i386`
+  ln -s "${LIBRARY_PATH}" "${RUST_PATH}/lib/rustlib/x86_64-apple-darwin/lib/libgfortran.dylib"
 fi
 
-BREW_PATH=`brew --prefix`
-RUST_PATH="${HOME}/rust"
-LIBRARY_PATH=`find "${BREW_PATH}" -name libgfortran.dylib | grep -v i386`
-ln -s "${LIBRARY_PATH}" "${RUST_PATH}/lib/rustlib/x86_64-apple-darwin/lib/libgfortran.dylib"
+gfortran --version
