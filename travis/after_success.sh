5b4b636a2d79d337803ed96339ea56ef08fec6d5
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-02 13:22:28 +0100

    Move the documentation-updating part into a separate script

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -2,28 +2,4 @@
 
 set -ev
 
-if [ ! -z "${RUSTDOC_VERSION}" ] && [ "${RUSTDOC_VERSION}" != "${TRAVIS_RUST_VERSION}" ]; then
-  exit
-fi
-
-git config user.name "Travis CI"
-git config user.email ""
-
-if [ -z "${CRATE_NAME}" ]; then
-  CRATE_NAME=$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2 | sed 's/-/_/g')
-fi
-
-URL="${CRATE_NAME}/index.html"
-
-HTML="<!DOCTYPE html>
-<link rel='canonical' href='${URL}'>
-<meta http-equiv='refresh' content='0; url=${URL}'>
-<script>window.location='${URL}'</script>"
-
-cargo doc ${CARGO_FLAGS}
-echo "${HTML}" > target/doc/index.html
-
-pip install ghp-import --user ${USER}
-${HOME}/.local/bin/ghp-import -m 'Update the documentation' -n target/doc
-
-git push -qf https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+curl https://stainless-steel.github.io/travis/update_documentation.sh | bash
