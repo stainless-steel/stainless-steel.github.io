0425da2fa0bed24aca862f760dcb7b6722dbf2ff
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-02 13:41:11 +0100

    Return the RUSTDOC_VERSION check

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -2,4 +2,8 @@
 
 set -ev
 
+if [ ! -z "${RUSTDOC_VERSION}" ] && [ "${RUSTDOC_VERSION}" != "${TRAVIS_RUST_VERSION}" ]; then
+  exit
+fi
+
 curl https://stainless-steel.github.io/travis/update_documentation.sh | bash
