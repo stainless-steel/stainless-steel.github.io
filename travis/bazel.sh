19f61a5da0e4258af2d6748f23052f960b10d93a
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-23 16:03:13 +0200

    Allow the Java switch to fail

--- travis/bazel.sh
+++ travis/bazel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -e
-
 BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/0.3.0/bazel-0.3.0-installer-linux-x86_64.sh
 JDK_SWITCHER_URL=https://raw.githubusercontent.com/michaelklishin/jdk_switcher/master/jdk_switcher.sh
 
 curl -LO ${JDK_SWITCHER_URL}
 source jdk_switcher.sh
 jdk_switcher use oraclejdk8
+
+set -e
 
 curl -o bazel.sh -L ${BAZEL_URL}
 chmod +x bazel.sh
