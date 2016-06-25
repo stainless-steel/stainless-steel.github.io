422691c174bca39ce4effbc34e9ef2a9cddcf314
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-25 09:48:03 +0200

    Adjust the Rust script

--- travis/rust.sh
+++ travis/rust.sh
@@ -5,7 +5,7 @@ set -e
 RUSTUP_URL=https://sh.rustup.rs
 
 curl ${RUSTUP_URL} -sSf | sh -s -- --default-toolchain ${TRAVIS_RUST_VERSION} -y
-export PATH="$HOME/.cargo/bin:${PATH}"
+export PATH="${HOME}/.cargo/bin:${PATH}"
 
 rustc --version
 cargo --version
