1733a02fff82f1a8b4999d2e4f4261adfc8eb601
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-09-26 12:11:03 +0200

    Update the bazel script

--- travis/bazel.sh
+++ travis/bazel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/0.3.0/bazel-0.3.0-installer-linux-x86_64.sh
+BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh
 JDK_SWITCHER_URL=https://raw.githubusercontent.com/michaelklishin/jdk_switcher/master/jdk_switcher.sh
 
 curl -LO ${JDK_SWITCHER_URL}
