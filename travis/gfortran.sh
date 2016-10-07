b71ff8b59c0ee9ae66aac9e6c97795dc223240cb
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-07 13:24:51 +0200

    Install Fortran

--- travis/gfortran.sh
+++ travis/gfortran.sh
@@ -3,10 +3,19 @@
 set -v
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-  BREW_PATH=$(brew --prefix)
+  ROOT_PATH="${PWD}/gfortran"
+  (
+      ARCHIVE_NAME="gcc-6.2-bin.tar.gz"
+      ARCHIVE_URL="http://prdownloads.sourceforge.net/hpc/${ARCHIVE_NAME}"
+      mkdir "${ROOT_PATH}" && cd "${ROOT_PATH}"
+      curl -LO "${ARCHIVE_URL}" && tar -xzf "${ARCHIVE_NAME}"
+  )
+  LIBRARY_NAME="libgfortran.dylib"
+  LIBRARY_PATH="${ROOT_PATH}/usr/local/lib"
+  export PATH="${ROOT_PATH}/usr/local/bin:${PATH}"
+  export DYLD_LIBRARY_PATH="${LIBRARY_PATH}:${DYLD_LIBRARY_PATH}"
   RUST_PATH=$(dirname $(dirname $(which rustc)))
-  LIBRARY_PATH=`find "${BREW_PATH}" -name libgfortran.dylib | grep -v i386`
-  ln -s "${LIBRARY_PATH}" "${RUST_PATH}/lib/rustlib/x86_64-apple-darwin/lib/libgfortran.dylib"
+  ln -s "${LIBRARY_PATH}/${LIBRARY_NAME}" "${RUST_PATH}/lib/rustlib/x86_64-apple-darwin/lib/${LIBRARY_NAME}"
 fi
 
 gfortran --version
