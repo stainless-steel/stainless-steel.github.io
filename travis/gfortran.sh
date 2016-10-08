647f407c436b4f1fbe572952f12d18b8cd0b7d25
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-08 07:38:02 +0200

    Uninstall Homebrew’s GCC

--- travis/gfortran.sh
+++ travis/gfortran.sh
@@ -3,6 +3,7 @@
 set -v
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+  brew uninstall gcc49
   curl -L http://prdownloads.sourceforge.net/hpc/gcc-6.2-bin.tar.gz -o archive.tar.gz
   mkdir root && tar -xzf archive.tar.gz -C root && cp -RLp root/ /
 fi
