038aa37e9169b3b981559c71d5c49b4492a56a08
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-15 07:19:59 +0200

    Use sudo when installing Fortran

--- travis/fortran.sh
+++ travis/fortran.sh
@@ -5,7 +5,7 @@ set -v
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     brew uninstall gcc49
     curl -L http://prdownloads.sourceforge.net/hpc/gcc-6.2-bin.tar.gz -o archive.tar.gz
-    mkdir root && tar -xzf archive.tar.gz -C root && cp -R root/ /
+    mkdir root && tar -xzf archive.tar.gz -C root && sudo cp -R root/* /
 fi
 
 gfortran --version
