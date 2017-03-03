b5e614ef7e70e7cb542ae5af8299275ac5031a44
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-03 06:11:12 +0100

    Use a different strategy for downloading gfortran

--- travis/fortran.sh
+++ travis/fortran.sh
@@ -4,8 +4,8 @@ set -v
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
   brew uninstall gcc49
-  while [ ! -f archive.tar.gz ]; do
-    curl -L http://prdownloads.sourceforge.net/hpc/gcc-6.2-bin.tar.gz -o archive.tar.gz
+  while true; do
+    curl -L http://prdownloads.sourceforge.net/hpc/gcc-6.2-bin.tar.gz -o archive.tar.gz && break
   done
   mkdir root && tar -xzf archive.tar.gz -C root && sudo cp -R root/* /
 fi
